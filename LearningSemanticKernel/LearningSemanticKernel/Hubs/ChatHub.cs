bb388b3ff3d13d184ba24dee7f6bb06da998196a
Mr-Bangladesh <faizul.cse4.bu@gmail.com>
2025-04-14 21:46:34 +0600

    bedrock ai connector added

--- LearningSemanticKernel/LearningSemanticKernel/Hubs/ChatHub.cs
+++ LearningSemanticKernel/LearningSemanticKernel/Hubs/ChatHub.cs
@@ -1,10 +1,19 @@
 ﻿using LearningSemanticKernel.Models;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
 
 namespace LearningSemanticKernel.Hubs;
 
 public class ChatHub : Hub
 {
+    private readonly IChatCompletionService _chatCompletionService;
+
+    public ChatHub(IChatCompletionService chatCompletionService)
+    {
+        _chatCompletionService = chatCompletionService;
+    }
+
     public async Task SendMessage(ChatMessageModel message)
     {
         message.ConnectionId = Context.ConnectionId;
@@ -16,5 +25,34 @@ public class ChatHub : Hub
     {
         await Clients.Client(message.ConnectionId)
             .SendAsync("ReceiveMessage", message.User, message.Message);
+
+        try
+        {
+            var res = _chatCompletionService.GetStreamingChatMessageContentsAsync(message.Message);
+
+            await StreamAsync(message.ConnectionId, res);
+        }
+        catch (Exception ex)
+        {
+            await Clients.Client(message.ConnectionId)
+                .SendAsync("ReceiveMessage", "Error", ex.Message);
+        }
+    }
+
+    private async Task StreamAsync(string connectionId, IAsyncEnumerable<StreamingChatMessageContent> res)
+    {
+        var first = true;
+        await foreach (var content in res.ConfigureAwait(false))
+        {
+            if (content.Role.HasValue && first)
+            {
+                await Clients.Client(connectionId)
+                    .SendAsync("ReceiveMessage", content.Role.ToString(), "");
+                first = false;
+            }
+
+            await Clients.Client(connectionId)
+                .SendAsync("ReceiveMessage", "", content.Content);
+        }
     }
 }
--- LearningSemanticKernel/LearningSemanticKernel/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ LearningSemanticKernel/LearningSemanticKernel/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,9 @@
-﻿namespace LearningSemanticKernel.Infrastructure.Extensions;
+﻿using Amazon;
+using Amazon.BedrockRuntime;
+using Amazon.Runtime;
+using Microsoft.SemanticKernel;
+#pragma warning disable SKEXP0070
+namespace LearningSemanticKernel.Infrastructure.Extensions;
 
 public static class ServiceCollectionExtensions
 {
@@ -10,6 +15,22 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddAIComponents(this IServiceCollection services, ConfigurationManager config)
     {
         services.AddKernel();
-        return services;
+        return services
+            .AddAmazonBedrock(config);
+    }
+
+    private static IServiceCollection AddAmazonBedrock(this IServiceCollection services, ConfigurationManager config)
+    {
+        var accessKey = config["AWS:AccessKey"];
+        var secretKey = config["AWS:SecretKey"];
+        var sessionToken = config["AWS:SessionToken"];
+        var region = config["AWS:Region"];
+        var modelId = config["AWS:ModelId"];
+
+        var cred = new SessionAWSCredentials(accessKey, secretKey, sessionToken);
+        var client = new AmazonBedrockRuntimeClient(cred, RegionEndpoint.GetBySystemName(region));
+
+        return services
+            .AddBedrockChatCompletionService(modelId, client);
     }
 }
